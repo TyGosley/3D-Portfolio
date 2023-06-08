855b85b0a32237a3d11f3b6d8accbdb36f857d3e
TyGosley <tygosley@gmail.com>
2023-06-08 15:00:10 -0700

    starter code and dependencies.  Started navbar.js

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
+const Navbar = () => {
+  const [active, setActive] = useState("");
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+        <div className="w-full flex items-center justify-between max-w-7x1 mx-auto">
+        <Link 
+           to="/"
+          className='flex items-center gap-2'
+          onClick={ () => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+          >
+            {/* TODO: Use own logo */}
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Tyler <span className='sm:block hidden'>|JavaScript Mastery </span></p>
+          </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
